63cde40ab6bce90925353ecd68d539a2d8c4966c
nimarahnamadev1996 <nimarahnamadev1996@gmail.com>
2025-10-19 11:29:24 +0330

    Home page - cart - function for + and  - product - Address

--- my-app/src/app/(private)/user/shop/page.tsx
+++ my-app/src/app/(private)/user/shop/page.tsx
@@ -1,15 +1,50 @@
-import { getCurrentUserFromSupabase } from '@/actions/users'
+import { getAllProducts } from '@/actions/products'
+import PageTitle from '@/components/ui/page-title';
 import React from 'react'
+import Filters from './_components/filters';
+import { IProduct } from '@/interfaces';
+import ProductTile from './_components/product-tile';
 
-const UserShopPage = async() => {
 
-    const user = await getCurrentUserFromSupabase()
+interface UserShopPageProps {
+  searchParams: Promise<{
+    category?: string;
+    searchText?: string;
+    sortBy?: string;
+  }>;
+}
+
+
+const UserShopPage = async({searchParams}: UserShopPageProps) => {
+
+  const { category, searchText, sortBy } = await searchParams
+
+  const response = await getAllProducts({
+    category: category || "",
+    searchText: searchText || "",
+    sortBy: sortBy || "",
+  })
+
+
+  if (!response.success) {
+     return <div>{response.message}</div>;
+  }
+
 
   return (
-    <div>
-        <h1>UserShopPage</h1>
-        <h3>Email: {user?.data?.email}</h3>
-        <p>Name: {user.data.name}</p>
+    <div className="flex flex-col gap-5">
+
+      <PageTitle title="Shop Products" />
+
+      <Filters/>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+        {
+         response.data?.map((product: IProduct) => (
+          <ProductTile  key={product.id} product={product}/>
+         )) 
+        }
+      </div>
     </div>
   )
 }
--- my-app/src/interfaces/index.tsx
+++ my-app/src/interfaces/index.tsx
@@ -11,3 +11,34 @@ export interface IUser {
   is_seller: boolean;
   created_at: string;
 }
+
+
+
+export interface IProduct {
+  id: number;
+  seller_id: string;
+  name: string;
+  category: string;
+  price: number;
+  available_stock: number;
+  description: string;
+  created_at: string;
+  images: string[];
+
+  // runtime properties
+  user_profiles: IUser;
+  quantity: number;
+}
+
+
+
+export interface IAddress {
+  id: number;
+  name: string;
+  email: string;
+  phone_number: number;
+  city: string;
+  state: string;
+  postal_code: number;
+  address: string;
+}
